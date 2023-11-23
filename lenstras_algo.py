fix bug with overflow in int type

lenstras_algo.py
@@ -23,9 +23,9 @@ class LenstraAlgorithm:
 
         Использование:
 
-        # >>>   n = int(...) -- любое целое число > 1
-        # >>>   lenstra_algo = LenstraAlgorithm(n)
-        # >>>   factors = lenstra_algo.factorize()
+        # >>> n = int(...) -- любое целое число > 1
+        # >>> lenstra_algo = LenstraAlgorithm(n)
+        # >>> factors = lenstra_algo.factorize()
 
         Parameters
         ----------
@@ -94,7 +94,7 @@ class LenstraAlgorithm:
         random_point = EllipticPoint(random_curve.x_point, random_curve.y_point, curve=random_curve)
         r_i = np.arange(2, self.omega_bound)
         m_i = np.floor(np.log(self.nu_bound + 2 * np.sqrt(self.nu_bound) + 1) / np.log(r_i))
-        max_power = int(prod(r_i ** m_i))
+        max_power = prod(r_i ** m_i)
         i = 1
         while i < max_power:
             i += 1